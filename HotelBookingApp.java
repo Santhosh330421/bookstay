947bfc8d9ead36f710b3172deb9a5baa35751f13
Santhosh330421 <santhoshkumaran.344@gmail.com>
2026-03-16 09:25:56 +0530

    Add files via upload

---
+++ HotelBookingApp.java
@@ -0,0 +1,8 @@
+
+public class HotelBookingApp {
+    public static void main(String[] args) {
+        System.out.println("     Welcome to Book My Stay App    ");
+        System.out.println("Application Name : Hotel Booking Management System");
+         System.out.println("Application started successfully!");
+          }
+        }
